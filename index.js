1d05d7851c19936e381f5b1dd309eb00e66866a8
Yathisha15 <119087843+Yathisha15@users.noreply.github.com>
2023-11-06 22:37:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfDrumButtons;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+        var buttonInnerHTML = this.innerHTML;
+        switch (buttonInnerHTML) {
+            case "w":
+                var crash = new Audio("./sounds/crash.mp3");
+                crash.play();
+                break;
+            case "a":
+                var kickBass = new Audio("./sounds/kick-bass.mp3");
+                kickBass.play();
+                break;
+            case "s":
+                var snare = new Audio("./sounds/snare.mp3");
+                snare.play();
+                break;
+            case "d":
+                var tom1 = new Audio("./sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case "j":
+                var tom2 = new Audio("./sounds/tom-2.mp3");
+                tom2.play();
+                break;
+            case "k":
+                var tom3 = new Audio("./sounds/tom-3.mp3");
+                tom3.play();ak;
+                
+        
+            default:
+                break;
+        }
+    });
+}
